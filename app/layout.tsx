10bba5aced803c82411eae8daf27af5bdc0576e6
carloscarvalho <misterchaty@outlook.com>
2026-03-27 09:40:17 -0300

    Removing Vercel Data Tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
